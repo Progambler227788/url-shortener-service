3ce977d292731cbbdb6ddb2afca02128c80e10e9
talha atif <talhaatif573@gmail.com>
2025-03-15 06:57:08 +0500

    added home controller

---
+++ src/main/java/com/talhaatif/urlshortener/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.talhaatif.urlshortener.controller;
+
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(
+        name = "Home Controller",
+        description =  "Welcome to URL shortening service!!"
+)
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String health(){
+        return "Let's use tickojet";
+    }
+}
